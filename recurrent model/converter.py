b814fe9cbeb665c136be756b3936c9b094235ab8
Tanmay Thakur <thakurtanmay72@yahoo.com>
2020-02-15 23:21:03 +0530

    Data Reshapen Irregularly, irregularities in model training

--- recurrent model/converter.py
+++ recurrent model/converter.py
@@ -13,23 +13,30 @@ from sklearn.preprocessing import StandardScaler
 
 data = pd.read_csv('features.csv')
 
+"""
 input_data = data.drop(['Date','Time'], axis = 1).values
+"""
 
-T = 60 
+input_data = data[['Vrms ph-n AN Avg','Vrms ph-n BN Avg','Vrms ph-n CN Avg']]
+
+T = 10 
 D = input_data.shape[1]
 N = len(input_data) - T 
 
-Ntrain = len(input_data) * 2//3
+Ntrain = len(input_data) * 4//5
+
+
 scaler = StandardScaler()
 scaler.fit(input_data[:Ntrain + T - 1])
 input_data = scaler.transform(input_data)
+
 
 X_train = np.zeros((Ntrain, T, D))
 Y_train = np.zeros((Ntrain, D))
 
 for t in range(Ntrain):
   X_train[t, :, :] = input_data[t:t+T]
-  Y_train[t] = input_data[t+T]
+  Y_train[t] = input_data.loc[t+T]
   
 print(X_train.shape, Y_train.shape)
 
@@ -47,4 +54,8 @@ data_dump = X_train, X_test, Y_train, Y_test
 
 pickle_out = open("dict.pickle","wb")
 pickle.dump(data_dump, pickle_out)
+pickle_out.close()
+
+pickle_out = open("scaler.pickle","wb")
+pickle.dump(scaler, pickle_out)
 pickle_out.close()
