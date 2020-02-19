51acf4ea36ad4bff4586fadbff9a3b65e39d0d7c
harshithvarma <40418673+harshithvarma@users.noreply.github.com>
2020-02-19 22:32:43 +0530

    Added generated pb file

--- authentication/proto.sh
+++ authentication/proto.sh
@@ -1,6 +1,6 @@
 echo "Generating grpc protofiles for Authentication Service"
 echo "Language: Golang"
-protoc -I ./proto ./proto/auth.proto --go_out=plugins=grpc:.
+protoc -I ./smartmeterpb ./smartmeterpb/auth.smartmeterpb --go_out=plugins=grpc:.
 if [ $? -eq 0 ]; then
   echo "Files Generated Successfully"
   else
