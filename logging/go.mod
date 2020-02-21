8d7c4ab7a33158009253834dab97aab31a4d76bf
Anshuman Chhapolia <achhap.10.01@gmail.com>
2020-02-21 21:28:16 +0530

    Logging Server Initialised
    Signed-off-by: Anshuman Chhapolia <achhap.10.01@gmail.com>

---
+++ logging/go.mod
@@ -0,0 +1,9 @@
+module github.com/spark/logging
+
+go 1.13
+
+require (
+	github.com/golang/protobuf v1.3.2
+	github.com/sirupsen/logrus v1.4.2
+	google.golang.org/grpc v1.27.1
+)
---
+++ logging/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	log "github.com/sirupsen/logrus"
+	"google.golang.org/grpc"
+	"io"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var s Server
+
+func main() {
+	lf, fileError := os.OpenFile("auth.log", os.O_RDWR, os.ModePerm)
+	if fileError != nil {
+		log.Warn("Error in opening logging file: auth.log")
+	}
+	s.logFile = lf
+
+	// setup logger
+	log.SetFormatter(&log.TextFormatter{
+		ForceColors:               false,
+		DisableColors:             false,
+		EnvironmentOverrideColors: false,
+		DisableTimestamp:          false,
+		FullTimestamp:             false,
+		TimestampFormat:           "",
+		DisableSorting:            false,
+		SortingFunc:               nil,
+		DisableLevelTruncation:    false,
+		QuoteEmptyFields:          false,
+		FieldMap:                  nil,
+		CallerPrettyfier:          nil,
+	})
+	mw := io.MultiWriter(os.Stdout, lf)
+	if fileError != nil {
+		mw = io.MultiWriter(os.Stdout)
+	}
+	log.SetOutput(mw)
+
+
+	// Setting up the server
+	log.Infoln("Starting Logging Server")
+	l, err := net.Listen("tcp", "0.0.0.0:8080")
+
+	if err != nil {
+		log.Fatalln("error in listening to the port 8080")
+	}
+
+	gs := grpc.NewServer()
+
+	// Start the server
+	go func() {
+		if err := gs.Serve(l); err != nil {
+			log.Fatalln("error in listening to port 8080")
+		}
+	}()
+
+	signalChannel := make(chan os.Signal)
+
+	signal.Notify(signalChannel, os.Interrupt, os.Kill, syscall.SIGTERM)
+	<-signalChannel
+	log.Info("Gracefully Shutting down the server")
+
+	// Shutdown Routine
+	s.CloseFile()
+}
